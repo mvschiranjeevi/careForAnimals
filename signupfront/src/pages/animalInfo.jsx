34ea2945249ea6880a6a1dbf56756584158746d6
kothaswapna <kothaswapna03@gmail.com>
2022-11-27 16:41:50 -0800

    Updated animals info page design

--- signupfront/src/pages/animalInfo.jsx
+++ signupfront/src/pages/animalInfo.jsx
@@ -1,15 +1,12 @@
-import { ChevronDownIcon } from "@chakra-ui/icons";
-import { Stack, Heading, Text, SimpleGrid, Button } from "@chakra-ui/react";
+import { Stack } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
-import Provider from "../chakra-theme/Provider";
-import ScrollIntoView from "react-scroll-into-view";
 import ResponsiveAppBar from "../components/navBar";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import ReactReadMoreReadLess from "react-read-more-read-less"
 
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -38,14 +35,12 @@ const AnimalInfo = props => {
 
   async function getAnimalInfo(name) {
   	try {
-  		console.log(name)
   		const response = await axios.get('http://localhost:4000/app/animalInfo', {
   			params: { animalName: "Asian Elephant" }}).then(
   				function (response) {
-  					console.log(response);
   					setAnimalData(response.data[0]);
-  					console.log(animalData);
   					setIsLoaded(true);
+  					console.log(response);
   				}
   			);
   	} catch(exception) {
@@ -58,8 +53,9 @@ const AnimalInfo = props => {
   		<ResponsiveAppBar />
 			{ isLoaded ? (
 				<Grid container spacing={1}>
-					<Grid item xs={12}>
-						<Item>
+				    <Grid item xs={1}></Grid>
+					<Grid item xs={5}>
+						<Item height="100%">
 							<Stack bgImage={animalData.imageUrl}
 								bgPosition="center"
 								bgRepeat="no-repeat"
@@ -72,15 +68,70 @@ const AnimalInfo = props => {
 							</Stack>
 						</Item>
 					</Grid>
-					<Grid item xs={4}>
+					<Grid item xs={5}>
+						<Item height="100%">
+						    <List sx={{ width: '100%', height: '30rem'}}>
+								<ListItem alignItems="flex-start" sx={{ width: '100%', height: '7.5rem', overflowY: 'scroll'}}>
+									<ListItemText
+										primary="WHY THEY MATTER"
+										secondary={
+											<React.Fragment>
+												<Typography
+													sx={{ display: 'inline' }}
+													component="span"
+													variant="body2"
+													color="text.primary"
+												>
+													<ReactReadMoreReadLess
+                                                        charLimit = {800}
+                                                        readMoreText = {"Read more ⬇"}
+                                                        readLessText = {"Read less ⬇"}
+                                                    >
+                                                        {animalData.threatInfo}
+                                                    </ReactReadMoreReadLess>
+												</Typography>
+											</React.Fragment>
+										}
+									/>
+								</ListItem>
+								<Divider component="li" />
+								<ListItem alignItems="flex-start" sx={{ width: '100%', height: '22.5rem', overflowY: 'scroll'}}>
+									<ListItemText
+										primary="ABOUT"
+										secondary={
+											<React.Fragment>
+												<Typography
+													sx={{ display: 'inline' }}
+													component="span"
+													variant="body2"
+													color="text.primary"
+												>
+													<ReactReadMoreReadLess
+                                                        charLimit = {800}
+                                                        readMoreText = {"Read more ⬇"}
+                                                        readLessText = {"Read less ⬆"}
+                                                    >
+                                                        {animalData.description}
+                                                    </ReactReadMoreReadLess>
+												</Typography>
+											</React.Fragment>
+										}
+									/>
+								</ListItem>
+							</List>
+						</Item>
+					</Grid>
+					<Grid item xs={1}></Grid>
+					<Grid item xs={1}></Grid>
+					<Grid item xs={2}>
 						<Item>
-							<List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+							<List sx={{ width: '100%', maxWidth: 360}}>
 								<ListItem alignItems="flex-start">
 									<ListItemAvatar>
-										<Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+										<Avatar src="/assets/icons/endangered.png" />
 									</ListItemAvatar>
 									<ListItemText
-										primary="Status"
+										primary="STATUS"
 										secondary={
 											<React.Fragment>
 												<Typography
@@ -96,14 +147,52 @@ const AnimalInfo = props => {
 									/>
 								</ListItem>
 								<Divider variant="inset" component="li" />
+								<ListItem alignItems="flex-start">
+									<ListItemAvatar>
+										<Avatar src="/assets/icons/population.png" />
+									</ListItemAvatar>
+									<ListItemText
+										primary="POPULATION"
+										secondary={
+											<React.Fragment>
+												<Typography
+													sx={{ display: 'inline' }}
+													component="span"
+													variant="body2"
+													color="text.primary"
+												>
+													{animalData.population}
+												</Typography>
+											</React.Fragment>
+										}
+									/>
+								</ListItem>
+								<Divider variant="inset" component="li" />
+								<ListItem alignItems="flex-start">
+									<ListItemAvatar>
+										<Avatar src="/assets/icons/sci.png" />
+									</ListItemAvatar>
+									<ListItemText
+										primary="SCIENTIFIC NAME"
+										secondary={
+											<React.Fragment>
+												<Typography
+													sx={{ display: 'inline' }}
+													component="span"
+													variant="body2"
+													color="text.primary"
+												>
+													{animalData.sciName}
+												</Typography>
+											</React.Fragment>
+										}
+									/>
+								</ListItem>
+								<Divider variant="inset" component="li" />
 							</List>
 						</Item>
 					</Grid>
-					<Grid item xs={8}>
-						<Item>
-							{animalData.description}
-						</Item>
-					</Grid>
+					<Grid item xs={1}></Grid>
 				</Grid>
 				) : (
 					<> </>
